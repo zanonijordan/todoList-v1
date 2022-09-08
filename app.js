b85bfbdbc8b3db9a961fc3348b20777273bc9603
JORDAN HENRIQUE MOREIRA ZANONI <52969396+zanonijordan@users.noreply.github.com>
2022-09-07 21:09:38 -0300

    update users

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({
 }))
 app.set('view engine', 'ejs');
 
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.gbmtnno.mongodb.net/todoListDB`, {
+mongoose.connect(`mongodb+srv://admin:B12RMR9k5MJE5wtC@cluster0.gbmtnno.mongodb.net/todoListDB`, {
   useNewUrlParser: true
 })
 // ---------------------------------itemSchema
